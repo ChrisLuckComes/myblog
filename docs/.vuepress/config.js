86e2b6fd935ee3bbb7e452e3fe91400570fbbfae
罗运来(Chris Luo)-顺丰科技 <Chris.Luo@sfmail.sf-express.com>
2020-06-17 17:58:59 +0800

    新增React目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -21,19 +21,19 @@ module.exports = {
           ["/CSS/锚点定位", "锚点定位"],
           [
             "/CSS/样式名很麻烦？使用postcss-alias偷懒",
-            "样式名很麻烦？使用postcss-alias偷懒"
+            "样式名很麻烦？使用postcss-alias偷懒",
           ],
           [
             "/CSS/sass共用代码 @include 和 @extend",
-            "sass共用代码 @include 和 @extend"
+            "sass共用代码 @include 和 @extend",
           ],
           [
             "/CSS/使用include-media简化媒体查询写法",
-            "使用include-media简化媒体查询写法"
+            "使用include-media简化媒体查询写法",
           ],
           [
             "/CSS/前端利器 - 使用iconfont管理图标",
-            "前端利器 - 使用iconfont管理图标"
+            "前端利器 - 使用iconfont管理图标",
           ],
           ["/CSS/滚动条样式", "滚动条样式"],
           ["/CSS/px转换rem和vw函数", "px转换rem和vw函数"],
@@ -41,8 +41,8 @@ module.exports = {
           ["/CSS/由等高布局引出的BFC概念", "由等高布局引出的BFC概念"],
           ["/CSS/实现长宽比恒定的元素", "实现长宽比恒定的元素"],
           ["/CSS/居中", "居中"],
-          ["/CSS/position", "position"]
-        ]
+          ["/CSS/position", "position"],
+        ],
       },
       {
         title: "Vue",
@@ -58,12 +58,16 @@ module.exports = {
           ["/Vue/history路由nginx配置", "history路由nginx配置"],
           ["/Vue/在客户端侧代码中使用环境变量", "在客户端侧代码中使用环境变量"],
           ["/Vue/es5,es6实现watch", "es5,es6实现watch"],
-          ["/Vue/es5,es6实现computed", "es5,es6实现computed"]
-        ]
+          ["/Vue/es5,es6实现computed", "es5,es6实现computed"],
+        ],
+      },
+      {
+        title: "React",
+        children: [["/React/路由+懒加载", "路由+懒加载"]],
       },
       {
         title: "Node.js",
-        children: [["/Node.js/路径总结", "路径总结"]]
+        children: [["/Node.js/路径总结", "路径总结"]],
       },
       {
         title: "JavaScript",
@@ -77,19 +81,19 @@ module.exports = {
           ["/JavaScript/继承的6种实现方式", "继承的6种实现方式"],
           [
             "/JavaScript/解读this的知识铺垫 - ECMA规范",
-            "解读this的知识铺垫 - ECMA规范"
+            "解读this的知识铺垫 - ECMA规范",
           ],
           ["/JavaScript/译文：解读this", "译文：解读this"],
           [
             "/JavaScript/实现同步和异步版本的sleep",
-            "实现同步和异步版本的sleep"
+            "实现同步和异步版本的sleep",
           ],
           [
             "/JavaScript/实现一个可以连续传参的累加函数",
-            "实现一个可以连续传参的累加函数"
+            "实现一个可以连续传参的累加函数",
           ],
-          ["/JavaScript/字符串字串操作", "字符串字串操作"]
-        ]
+          ["/JavaScript/字符串字串操作", "字符串字串操作"],
+        ],
       },
       {
         title: "前端",
@@ -101,16 +105,16 @@ module.exports = {
           ["/前端/语义化", "语义化"],
           [
             "/前端/script标签中async和defer的区别",
-            "script标签中async和defer的区别"
+            "script标签中async和defer的区别",
           ],
           ["/前端/https原理及流程", "https原理及流程"],
           ["/前端/xss攻击", "xss攻击"],
-          ["/前端/Event Loop", "Event Loop"]
-        ]
+          ["/前端/Event Loop", "Event Loop"],
+        ],
       },
       {
         title: "数据结构",
-        children: [["/数据结构/链表", "链表"]]
+        children: [["/数据结构/链表", "链表"]],
       },
       {
         title: "Webpack",
@@ -118,12 +122,12 @@ module.exports = {
           ["/Webpack/HelloWorld", "HelloWorld"],
           ["/Webpack/Loader", "Loader"],
           ["/Webpack/Plugin", "Plugin"],
-          ["/Webpack/devServer", "devServer"]
-        ]
+          ["/Webpack/devServer", "devServer"],
+        ],
       },
       {
         title: "flutter",
-        children: [["/flutter/windows环境安装与配置", "windows环境安装与配置"]]
+        children: [["/flutter/windows环境安装与配置", "windows环境安装与配置"]],
       },
       {
         title: "正则表达式",
@@ -131,9 +135,9 @@ module.exports = {
           ["/正则表达式/金额格式化", "金额格式化"],
           ["/正则表达式/实现模板字符串", "实现模板字符串"],
           ["/正则表达式/表单校验", "表单校验"],
-          ["/正则表达式/匹配规则速查表", "匹配规则速查表"]
-        ]
-      }
-    ]
-  }
+          ["/正则表达式/匹配规则速查表", "匹配规则速查表"],
+        ],
+      },
+    ],
+  },
 };
